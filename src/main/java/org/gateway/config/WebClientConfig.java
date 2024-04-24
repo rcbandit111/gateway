0f610572797cec52c04a4ef41f1a6e7f72406756
Mona Lisa <pterziev@datexcorp.com>
2024-04-24 14:28:19 +0300

    Updated code

--- src/main/java/org/gateway/config/WebClientConfig.java
+++ src/main/java/org/gateway/config/WebClientConfig.java
@@ -2,7 +2,9 @@ package org.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,23 +22,30 @@ public class WebClientConfig {
 //    }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain (ServerHttpSecurity http) {
-
-        http.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers("/api/**")
-                .permitAll();
-//                .anyExchange()
-//                .authenticated()
-//                .and()
-//                .oauth2Login(); // to redirect to oauth2 login page.
-        http.cors().configurationSource(request-> {
-            CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-            configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-            configuration.setAllowedHeaders(List.of("*"));
-            return configuration;
-        });
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
     }
+
+
+
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain (ServerHttpSecurity http) {
+//
+//        http.csrf().disable()
+//                .authorizeExchange()
+//                .pathMatchers("/api/**")
+//                .permitAll();
+////                .anyExchange()
+////                .authenticated()
+////                .and()
+////                .oauth2Login(); // to redirect to oauth2 login page.
+//        http.cors().configurationSource(request-> {
+//            CorsConfiguration configuration = new CorsConfiguration();
+//            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//            configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+//            configuration.setAllowedHeaders(List.of("*"));
+//            return configuration;
+//        });
+//        return http.build();
+//    }
 }
