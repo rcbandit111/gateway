172376c4c0e8b23a59c10ae41fba6b315a70bffd
Mona Lisa <pterziev@datexcorp.com>
2024-04-25 21:17:22 +0300

    Updated code

--- src/main/java/org/gateway/Application.java
+++ src/main/java/org/gateway/Application.java
@@ -3,8 +3,11 @@ package org.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -14,6 +17,24 @@ import java.util.Collections;
 @SpringBootApplication(scanBasePackages = {"org.gateway"}, exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class Application {
+
+    DiscoveryClient discoveryClient;
+
+    public Application(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void testing() {
+
+        int size = discoveryClient.getServices().size();
+        System.out.println("Starting EventListener when ApplicationReadyEvent !!!");
+        System.out.println("Kubernetes services size " + size);
+        discoveryClient.probe();
+        for (String service : discoveryClient.getServices()) {
+            System.out.println("Discovered service " + service);
+        }
+    }
 
     public static void main(final String[] args)
     {
